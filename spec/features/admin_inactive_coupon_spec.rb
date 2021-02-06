4ff995c650eed826858c7fe55fdfcac0b8a1466e
Milena Ferreira <milena.neeves@gmail.com>
2021-02-06 18:27:58 -0300

    Inativar cupom

--- spec/features/admin_inactive_coupon_spec.rb
+++ spec/features/admin_inactive_coupon_spec.rb
@@ -5,13 +5,47 @@ feature "Admin inactivate coupon" do
     promotion = Promotion.create!(name: "Natal", description: "", code: "NATAL10",
                                   coupon_quantity: 100, discount_rate: 10,
                                   expiration_date: "2021-10-10")
-    promotion.coupons.create!(code: "NATAL10-0030")
+    coupon = Coupon.create!(code: "NATAL10-0030", promotion: promotion)
 
     visit root_path
     click_on "Promoções"
     click_on promotion.name
     click_on "Inativar"
 
+    coupon.reload
     expect(page).to have_content("NATAL10-0030 (Inativo)")
+    expect(coupon).to be_inactive
+  end
+
+  scenario "does not view button" do
+    promotion = Promotion.create!(name: "Natal", description: "", code: "NATAL10",
+                                  coupon_quantity: 100, discount_rate: 10,
+                                  expiration_date: "2021-10-10")
+    inactive_coupon = Coupon.create!(code: "NATAL10-0030", promotion: promotion, status: :inactive)
+    active_coupon = Coupon.create!(code: "NATAL10-0031", promotion: promotion, status: :active)
+
+    visit root_path
+    click_on "Promoções"
+    click_on promotion.name
+
+    expect(page).to have_content("NATAL10-0030 (Inativo)")
+    expect(page).to have_content("NATAL10-0031 (Ativo)")
+
+    within("div#coupon-#{active_coupon.id}") do
+      expect(page).to have_link("Inativar")
+    end
+
+    within("div#coupon-#{inactive_coupon.id}") do
+      expect(page).not_to have_link("Inativar")
+    end
+  end
+
+  scenario "inactive request can be made multiple times", type: :request do
+    promotion = Promotion.create!(name: "Natal", description: "", code: "NATAL10",
+                                  coupon_quantity: 100, discount_rate: 10,
+                                  expiration_date: "2021-10-10")
+    inactive_coupon = Coupon.create!(code: "NATAL10-0030", promotion: promotion, status: :inactive)
+
+    expect { post inactivate_coupon_path(inactive_coupon) }.not_to change { inactive_coupon.reload.updated_at }
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-RSpec.describe Category, type: :model do
+describe Category, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:code) }
-    it { should validate_uniqueness_of(:code) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:code) }
+    it { is_expected.to validate_uniqueness_of(:code) }
   end
 end
