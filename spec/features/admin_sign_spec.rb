7b301c52c9805a0392bbd595e35241d3a98b1c59
Milena Ferreira <milena.neeves@gmail.com>
2021-02-08 15:41:02 -0300

    Admin se cadastra na aplicacao

--- spec/features/admin_sign_spec.rb
+++ spec/features/admin_sign_spec.rb
@@ -1,6 +1,37 @@
 require "rails_helper"
 
 feature "Admin sign in" do
+  scenario "sign up" do
+    visit root_path
+    click_on "Registrar-se"
+    within("form") do
+      fill_in "E-mail", with: "milena@email.com"
+      fill_in "Senha", with: "123456"
+      fill_in "Confirme sua senha", with: "123456"
+      click_on "Registrar"
+    end
+
+    expect(page).to have_content("milena@email.com")
+    expect(page).to have_content("Bem vindo! Você realizou seu registro com sucesso")
+    expect(page).to have_link("Sair")
+    expect(page).not_to have_link("Entrar")
+  end
+
+  scenario "and attributes cannot be blank" do
+    visit root_path
+    click_on "Registrar-se"
+    within("form") do
+      fill_in "E-mail", with: ""
+      fill_in "Senha", with: ""
+      fill_in "Confirme sua senha", with: ""
+      click_on "Registrar"
+    end
+
+    expect(page).to have_content("E-mail não pode ficar em branco")
+    expect(page).to have_content("Senha não pode ficar em branco")
+  end
+
+
   scenario "successfully" do
     admin = Admin.create!(email: "milena@email.com", password: "123456")
 
